5901001d9ff8108beb8e2da7c61409782bff601e
Eric Hopkins <scarecrow3322@gmail.com>
2014-05-26 10:49:55 -0700

    wild pokemon constructor: first draft
    Made some small changes to the pokemon class. As well as crated a "rough
    draft" of a constructor for wild pokemon.

--- Pokemon/cs/Pokemon.cs
+++ Pokemon/cs/Pokemon.cs
@@ -29,12 +29,43 @@ namespace Pokemon
         int Gender { get; set; }
         int Level { get; set; }
         int XP { get; set; }
-        int[] stats = new int[6];
+        int[] baseStats = new int[6];
         int[] EVs = new int[6];
         int[] IVs = new int[6];
+        int[] stats = new int[6];
         string Name { get; set; }
+        // TODO: create Abilities class
         // TODO: create Attacks class
         // TODO: create LevelGroup class
         // TODO: create Type class
+
+        // Constructor: Wild Pokemon
+        Pokemon(Pokemon basePkm, int areaLow, int areaHigh)
+        {
+            Random rnd = new Random();
+            
+            this.isGenderless = basePkm.isGenderless;
+            this.CatchRate = basePkm.CatchRate;
+            this.DexNumber = basePkm.DexNumber;
+            this.Name = basePkm.Name;
+            for(int i = 0; i < 6; i++)
+            {
+                this.baseStats[i] = basePkm.baseStats[i];
+            }
+
+            // Create generated data
+            for (int i = 0; i < 6; i++)
+            {
+                // NOTE: I don't consider this a final product, I think the
+                // IV's should have more to them than just pure random numbers
+
+                this.IVs[i] = rnd.Next(31);
+            }
+
+            // TODO: When XP is properly implemented, this should be changed.
+            this.Level = rnd.Next(areaLow, areaHigh);
+
+            this.Gender = rnd.Next(1, 2);
+        }
     }
 }
