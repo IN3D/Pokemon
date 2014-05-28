34b1b3c267859c5fb902b2952aad64c1e2d268ed
Bradley Mackey <bradmackey2013@live.com>
2014-05-28 16:17:42 -0400

    Types complete
    closes #10

---
+++ Pokemon/cs/Types.cs
@@ -0,0 +1,27 @@
+﻿/* Pokemon: Infinte: A recreation of classic Pokemon games in C#
+ * 
+ * 
+ * == Created ==
+ * on: 5/28/2014
+ * by: Bradley Mackey
+ * 
+ * This is the multipliers for damage output based on type versus type.
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon.cs
+{
+     public class types
+    {
+        protected string name { get; set;}
+        protected double[] modifiers = new double[18];
+
+     
+       
+    }
+}
