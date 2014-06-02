e7e4d776a3073ef872aa9773bfd1815d63ffbaa4
BradMackey <bradmackey2013@live.com>
2014-06-01 21:21:22 -0400

    Pokeball Class test

--- Pokemon/Program.cs
+++ Pokemon/Program.cs
@@ -10,6 +10,20 @@ namespace Pokemon
     {
         static void Main(string[] args)
         {
+            LinkedList<Pokeball_Class> objects = new LinkedList<Pokeball_Class>();
+
+            generic pokeball = new generic("Pokeball", "*A flash occurs as the Pokemon appears*", " is a device for catching wild Pokémon. It's thrown like a ball at a Pokémon, comfortably encapsulating its target.", 1);
+            great greatball = new great("Greatball", "*A flash occurs as the Pokemon appears*", " is a good, high-performance Poké Ball that provides a higher Pokémon catch rate than a standard Poké Ball can.", 1.5);
+
+            objects.AddLast(pokeball);
+            objects.AddLast(greatball);
+
+            foreach (var item in objects)
+            {
+                Console.WriteLine(item.use());
+            }
+
+            Console.ReadLine();
         }
     }
 }
---
+++ Pokemon/cs/Pokeball-Class.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon
+{
+    abstract class Pokeball_Class
+    {
+        protected string Name { get; set; }
+        protected string Description {get; set; }
+        protected string PokeballFlash { get; set; }
+        protected double CatchRate { get; set; }
+
+        public virtual string use()
+        {
+            return "Not Implemented";
+        }
+    }
+
+    class generic : Pokeball_Class
+    {
+        public generic (string name, string flash, string description, double catchRate)
+        {
+            this.Name = name;
+            this.PokeballFlash = flash;
+            this.Description = description;
+            this.CatchRate = catchRate;
+
+        }
+
+        public override string use()
+        {
+           return this.Name + this.Description +" Catch rate of: " + this.CatchRate + "x" + "\n" + this.PokeballFlash;
+        }
+    }
+
+    class great : Pokeball_Class
+    {
+        public great (string name, string flash, string description, double catchRate)
+        {
+            this.Name = name;
+            this.PokeballFlash = flash;
+            this.Description = description;
+            this.CatchRate = catchRate;
+        }
+
+        public override string use()
+        {
+            return this.Name + this.Description + " Catch rate of: " + this.CatchRate + "x" + "\n" + this.PokeballFlash;
+        }
+    }
+}
