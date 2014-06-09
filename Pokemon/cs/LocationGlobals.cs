c3300487c121aadb4aa8c1049893bc6fd619a3a9
Eric Hopkins <scarecrow3322@gmail.com>
2014-06-09 18:18:40 -0400

    Classes in place to start building interaction
    Everything should be in place that code can start to be written to
    implement actual player interaction.

---
+++ Pokemon/cs/LocationGlobals.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Pokemon
+{
+	public static class LocationGlobals
+	{
+		public static int CurrentRegion { get; set; }
+		public static int CurrentArea { get; set; }
+		public static int CurrentBuilding {get; set; }
+		public static int CurrentRoom { get; set; }
+	}
+}
+
---
+++ Pokemon/cs/Region.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pokemon
+{
+	public class Region
+	{
+		public LinkedList<Area> areas = new LinkedList<Area>();
+
+		public Region ()
+		{
+			// Constructor doesn't need to do anything
+		}
+	}
+}
+
