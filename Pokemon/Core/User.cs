f91f49148efa47ebcedbcb229ce38ac69df165ca
Eric Hopkins <scarecrow3322@gmail.com>
2014-08-18 21:21:00 -0700

    added core and model data
    This adds in the necessary C# class files for working with the login.
    One is a class that encapsulates the the MongoDB client, and the other
    is the class for the user login.

---
+++ Pokemon/Core/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+
+namespace Pokemon.Core
+{
+    class User
+    {
+        public ObjectId id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public bool Developer { get; set; }
+
+        public User()
+        {
+
+        }
+    }
+}
---
+++ Pokemon/Model/Client.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Driver.Communication;
+using MongoDB.Driver.Linq;
+using MongoDB.Bson;
+
+namespace Pokemon.Model
+{
+    class Client
+    {
+        MongoClient client;
+        MongoServer server;
+
+        public Client()
+        {
+            client = new MongoClient("mongodb://Application:mm3LhteFxx8Q@ds063449.mongolab.com:63449/pokemon");
+            server = client.GetServer();
+        }
+
+        public MongoCollection<T> getCollection<T> (string collectionName)
+        {
+            var db = server.GetDatabase("pokemon");
+            MongoCollection<T> collection = db.GetCollection<T>(collectionName);
+            return collection;
+        }
+        
+    }
+}
