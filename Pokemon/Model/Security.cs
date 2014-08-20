cb0c95d342aea25e0ea071ae9312f3ca169b8ce0
Eric Hopkins <scarecrow3322@gmail.com>
2014-08-20 14:11:57 -0700

    Began adding some security features to the login
    I've decided that I'm going to hash and salt user passwords, so I've
    taken steps to begin doing that. Users now have a salt value stored.

---
+++ Pokemon/Model/Security.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon.Model
+{
+    public static class Security
+    {
+        public static string generateHash(string password)
+        {
+            byte[] data = System.Text.Encoding.ASCII.GetBytes(password);
+            data = new System.Security.Cryptography.SHA256Managed().ComputeHash(data);
+            return System.Text.Encoding.ASCII.GetString(data);
+        }
+
+        public static string getSalt(int size)
+        {
+            System.Security.Cryptography.RNGCryptoServiceProvider rng = new System.Security.Cryptography.RNGCryptoServiceProvider();
+            byte[] buff = new byte[size];
+            rng.GetBytes(buff);
+
+            return Convert.ToBase64String(buff);
+        }
+    }
+}
