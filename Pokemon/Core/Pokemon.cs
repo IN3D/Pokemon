7727c57b61cee5278a125a937917b4367ec18de5
Eric Hopkins <scarecrow3322@gmail.com>
2014-08-28 15:20:14 -0700

    defined the unique pokemon class
    This is the class that will be used to define a particular pokemon and
    what makes it unique.

---
+++ Pokemon/Core/Pokemon.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * == Created ==
+ * on: 08/28/14
+ * by: Eric Hopkins
+ * 
+ * This class defines specific (ie. unique) pokemon.
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+
+namespace Pokemon.Core
+{
+    class Pokemon
+    {
+        public ObjectId id { get; set; }
+        public ObjectId OriginalTrainerID { get; set; }
+        public int Ability { get; set; }
+        public int Gender { get; set; }
+        public string Nickname { get; set; }
+        public int Level { get; set; }
+        public int XPToNextLevel { get; set; }
+        public int XPCurrent { get; set; }
+        public string BallType { get; set; }
+
+        protected BasePokemon basePkm;
+        protected int[] EVs = new int[6];
+        protected int[] IVs = new int[6];
+        protected int[] Stats = new int[6];
+    }
+}
